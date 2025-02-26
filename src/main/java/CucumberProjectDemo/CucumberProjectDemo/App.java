f9e8a553e826a969faae4fbfd7fc5797600ce0a8
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-02-25 19:22:45 -0500

    Added features for Login and Graph

--- src/main/java/CucumberProjectDemo/CucumberProjectDemo/App.java
+++ src/main/java/CucumberProjectDemo/CucumberProjectDemo/App.java
@@ -1,13 +1,55 @@
 package CucumberProjectDemo.CucumberProjectDemo;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
         System.out.println( "Hello World!" );
+        
+        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+		driver.get("https://dsportalapp.herokuapp.com/login");
+		driver.manage().window().maximize();
+		//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("bugbusters");
+		//driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Team@bug");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+		
+        
+		
+		WebElement warningMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body")));
+
+		//System.out.println(warningMessage.getText());
+		
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+        String outerHTML = (String) js.executeScript("return arguments[0].outerHTML;", warningMessage);
+        
+       // System.out.println(outerHTML);
+
+		
+		
+					
+	
+	
+			
     }
-}
+    
+  }
--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,74 +1,162 @@
 package stepDefinitions;
 
+import java.time.Duration;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.LoginPage;
 import utilities.Helper;
 
 public class LoginSteps extends BaseClass {
+	
+	WebDriver driver = new ChromeDriver();
+	
  
- @Given("User Launches Chrome Browser")
- public void user_launches_chrome_browser() {
-  loginPage = new LoginPage(Helper.getDriver());   
+ @Given("The user is on the DS Algo Home Page")
+ public void the_user_is_on_the_ds_algo_home_page() {
+     // Write code here that turns the phrase above into concrete actions	 
+	 	WebDriverManager.chromedriver().setup();
+		driver.get("https://dsportalapp.herokuapp.com/home");
+		driver.manage().window().maximize();
+		
+      
  }
 
- @When("user opens facebook  portal link {string}")
- public void user_opens_facebook_portal_link(String url) {
-    Helper.openPage(url);    
- }
- 
- @Then("User should see the  page title {string}")
- public void user_should_see_the_page_title(String string) {
-   String loginPgTitle = Helper.getTitle();
-   Assert.assertEquals(string, loginPgTitle); 
- }
- 
- @When("User enters Username as {string} and Password as {string}")
- public void user_enters_username_as_and_password_as(String userName, String password) {
-    if(userName.equalsIgnoreCase("Valid") && password.equalsIgnoreCase("Valid"))
-    {
-    userName= Helper.GetUserName();
-    password= Helper.GetPassword();
-    }
-    loginPage.SetUserName(userName);
-    loginPage.SetPassword(password);  
-  
-  System.out.println("User Name: "+ userName);
-  System.out.println("Password: "+ password);
+ @When("The user should click the Sign in link")
+ public void the_user_should_click_the_sign_in_link() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//*[text()='Sign in']")).click();
  }
 
- @Then("User clicks on Login button with expected status as {string}")
- public void user_clicks_on_login_button(String expectedStatus) {
-	 loginPage.ClickBtnLogin();
-  
-  if (expectedStatus == "Both Fail")
-  {
-   String userNameValMessage = loginPage.getErrMsgUN();
-   String pwdValMessage= loginPage.getErrMsgUN();
-   
-   Assert.assertEquals("Please enter your user name", userNameValMessage);
-   Assert.assertEquals("Please enter your password", pwdValMessage);
-   return;
-  }
-  else if (expectedStatus == "Missing Password")
-  {
-   String commonMsg =loginPage.getErrMsgPWD();
-   Assert.assertEquals("Invalid username and password Please try again",commonMsg);
-   return;
-  }
+ @Then("The user should be redirected to Sign in page")
+ public void the_user_should_be_redirected_to_sign_in_page() {
+     // Write code here that turns the phrase above into concrete actions
+	 String loginPgTitle = driver.getTitle();
+	 Assert.assertEquals("Login", loginPgTitle);
+      
+ }
+
+ @Given("The user is on the DS Algo Sign in Page")
+ public void the_user_is_on_the_ds_algo_sign_in_page() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	 driver.manage().window().maximize();
+      
+ }
+
+ @When("The user clicks login button after leaving the {string} textbox and {string} textbox empty")
+ public void the_user_clicks_login_button_after_leaving_the_textbox_and_textbox_empty(String string, String string2) {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+ }
+
+ @Then("The error message {string} appears below Username textbox")
+ public void the_error_message_appears_below_username_textbox(String string) {
+     // Write code here that turns the phrase above into concrete actions 
+	// WebElement ErrMsg = driver.findElement(By.xpath("//*[contains(text(),'Please fill out this field')]"));
+	// Assert.assertEquals(string, ErrMsg.getText());
+ }
+
+ @When("The user clicks login button after entering the {string} and leaves {string} textbox empty")
+ public void the_user_clicks_login_button_after_entering_the_and_leaves_textbox_empty(String string, String string2) {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@name='username']")).sendKeys(string);
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+      
+ }
+
+ @Then("The error message {string} appears below Password textbox")
+ public void the_error_message_appears_below_password_textbox(String string) {
+     // Write code here that turns the phrase above into concrete actions
+	 //WebElement msg = driver.findElement(By.xpath("//div[@class='alert alert-primary']"));
+	 //Assert.assertEquals(string, msg.getText());
+ }
+
+ @When("The user clicks login button after entering the Password only")
+ public void the_user_clicks_login_button_after_entering_the_password_only() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("password");
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+      
+ }
+
+ @When("The user clicks login button after entering invalid username and valid password")
+ public void the_user_clicks_login_button_after_entering_invalid_username_and_valid_password() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Username");
+	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("password");
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
  }
  
- @Then("User should see the Facebook Home page on successful login status {string}")
- public void user_should_see_the_facebook_home_page_on_successful_login_status(String status) throws InterruptedException { 
-  
- 
-  if (status.equalsIgnoreCase("Pass"))
-  {
-   Assert.assertEquals(loginPage.getHomePgTitle(), "Facebook");
-   System.out.println("Login Passed");
-   Assert.assertEquals(Helper.getTitle(), "facebook");
-  } 
+ @Then("The user should able to see an error message {string}.")
+ public void the_user_should_able_to_see_an_error_message(String string) {
+     // Write code here that turns the phrase above into concrete actions
+	 //WebElement msg = driver.findElement(By.xpath("//div[@class='alert alert-primary']"));
+	// Assert.assertEquals(string, msg.getText());
  }
+ 
+ @When("The user clicks login button after entering Valid username and Incorrect password")
+ public void the_user_clicks_login_button_after_entering_valid_username_and_incorrect_password() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@name='username']")).sendKeys("bugbusters");
+	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("password");
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+ }
+
+ @When("The user clicks login button after entering valid username and valid password")
+ public void the_user_clicks_login_button_after_entering_valid_username_and_valid_password() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.findElement(By.xpath("//input[@name='username']")).sendKeys("bugbusters");
+	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Team@bug");
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+ }
+
+ @Then("The user should land in Data Structure Home Page with message {string}")
+ public void the_user_should_land_in_data_structure_home_page_with_message(String string) {
+     // Write code here that turns the phrase above into concrete actions
+	 //WebElement msg = driver.findElement(By.xpath("//div[@class='alert alert-primary']"));
+	// Assert.assertEquals(string, msg.getText());
+ }
+
+ @Given("The user is in the Home page after Sign in")
+ public void the_user_is_in_the_home_page_after_sign_in() {
+     // Write code here that turns the phrase above into concrete actions
+	 driver.get("https://dsportalapp.herokuapp.com/login");
+	 driver.manage().window().maximize();
+	 driver.findElement(By.xpath("//input[@name='username']")).sendKeys("bugbusters");
+	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Team@bug");
+	 driver.findElement(By.xpath("//input[@type='submit']")).click();
+ }
+
+ @When("The user clicks {string}")
+ public void the_user_clicks(String string) {
+     // Write code here that turns the phrase above into concrete actions	
+	 WebDriverWait wait;
+	 wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	 wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[text()='Sign out']"))).click();
+ }
+
+ @Then("The user should be redirected to home page with message {string}")
+ public void the_user_should_be_redirected_to_home_page_with_message(String string) {
+     // Write code here that turns the phrase above into concrete actions
+	 WebElement msg = driver.findElement(By.xpath("//div[@class='alert alert-primary']"));
+	 Assert.assertEquals(string, msg.getText());
+ }
+
+
+
+ 
 }
