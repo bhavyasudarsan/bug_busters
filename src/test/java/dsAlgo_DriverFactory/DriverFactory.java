d43815e376e0a70d79dddc0bea3bf32da00a3bdc
SonuBavadhasan <sonubdas@gmail.com>
2025-03-12 13:24:55 -0400

    Added ConfigReader

--- src/test/java/dsAlgo_DriverFactory/DriverFactory.java
+++ src/test/java/dsAlgo_DriverFactory/DriverFactory.java
@@ -3,43 +3,39 @@ package dsAlgo_DriverFactory;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverFactory {
 
 	public static WebDriver driver;
+	
 	public final static int TIMEOUT = 2;
 
-	public static WebDriver initiateDriver() {
-
-// WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	public WebDriver driverSetup(String browser) {
+		if(browser.equalsIgnoreCase("Chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+		}
 		new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
 		driver.manage().window().maximize();
 		return driver;
 	}
-
-	public WebDriver getDriver() {
+	
+	public static WebDriver getDriverInstance() {
 		return driver;
+		
 	}
 
-	public static void openApp() {
-		driver.get("https://dsportalapp.herokuapp.com/");
-	}
-
-	public static void openHomeScreen() {
-		driver.get("https://dsportalapp.herokuapp.com/home");
-	}
-
-	public static String getTitle() {
+	public String getTitle() {
 		return driver.getTitle();
-	}
-
-	public static void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
 	}
 
 }
--- src/test/java/dsAlgo_hooks/Hooks.java
+++ src/test/java/dsAlgo_hooks/Hooks.java
@@ -1,5 +1,30 @@
 package dsAlgo_hooks;
 
-public class Hooks {
+import org.openqa.selenium.WebDriver;
 
+import dsAlgo_DriverFactory.DriverFactory;
+import dsAlgo_Utilities.ConfigReader;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+	
+	DriverFactory driverFactory = new DriverFactory();
+	ConfigReader configReader = new ConfigReader();
+	WebDriver driver;
+	String url;
+	
+	@Before
+	public void setup() {
+		driver=driverFactory.driverSetup(configReader.getBrowser());
+		url=configReader.getUrl();
+		driver.get(url);
+	}
+
+	@After
+	public void closeDriver() {
+			if (driver != null) {
+				driver.quit();
+			}
+	}
 }
