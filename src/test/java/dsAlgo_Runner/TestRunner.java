efee44a9a1751ebc2f75c34d7810161b46a7db00
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-03-15 21:13:29 -0400

    updated pom.xml

--- src/test/java/dsAlgo_Runner/TestRunner.java
+++ src/test/java/dsAlgo_Runner/TestRunner.java
@@ -9,9 +9,9 @@ import io.cucumber.junit.CucumberOptions;
 		@RunWith(Cucumber.class)
 		@CucumberOptions(
 				plugin = {"pretty", "html:target/dsAlgo.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
+				         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",}, //reporting purpose
 				monochrome=false,  //console output color
-				tags = "@Login",  //tags from feature file
+				//tags = "@Login",  //tags from feature file
 				features = "src/test/resources/dsAlgo_FeatureFiles", //location of feature files
 				glue= {"dsAlgo_StepDefinition", "dsAlgo_hooks" }) //location of step definition files
 
