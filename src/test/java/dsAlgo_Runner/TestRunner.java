3fa7d1d4a1b1acfd89c62f1ce63010234c6000a5
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-03-17 08:35:34 -0400

    Updated TestRunner with TestNG

--- src/test/java/dsAlgo_Runner/TestRunner.java
+++ src/test/java/dsAlgo_Runner/TestRunner.java
@@ -1,21 +1,27 @@
 package dsAlgo_Runner;
 
 
-import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
 
-		@RunWith(Cucumber.class)
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+	
 		@CucumberOptions(
 				plugin = {"pretty", "html:target/dsAlgo.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 				         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
 				monochrome=false,  //console output color
-				tags = "@Login or @linkedTest or @stackTest or @homeTest or @treeTest or @arrayTest",  //tags from feature file
+				tags = "@Login",  //tags from feature file
 				features = "src/test/resources/dsAlgo_FeatureFiles", //location of feature files
 				glue= {"dsAlgo_StepDefinition", "dsAlgo_hooks" }) //location of step definition files
-
-
-public class TestRunner {
-				
+		
+public class TestRunner extends AbstractTestNGCucumberTests{
+			/*@Override
+			@DataProvider(parallel = true)
+			public Object[][] scenarios()
+			{
+				return super.scenarios();
+			}	*/	
 }
