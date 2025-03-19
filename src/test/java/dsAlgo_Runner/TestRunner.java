3385eb050054677ce4be35ec71772e71122cee32
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-03-18 20:41:56 -0400

    Test Runner as per main

--- src/test/java/dsAlgo_Runner/TestRunner.java
+++ src/test/java/dsAlgo_Runner/TestRunner.java
@@ -1,27 +1,25 @@
 package dsAlgo_Runner;
 
-
-
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-import org.testng.annotations.DataProvider;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-	
 		@CucumberOptions(
 				plugin = {"pretty", "html:target/dsAlgo.html", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-				         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
-				monochrome=false,  //console output color
-				tags = "@Login",  //tags from feature file
-				features = "src/test/resources/dsAlgo_FeatureFiles", //location of feature files
-				glue= {"dsAlgo_StepDefinition", "dsAlgo_hooks" }) //location of step definition files
+				         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
+				monochrome=false,  
+				tags = "@Login or @linkedTest or @stackTest or @homeTest or @treeTest or @arrayTest",  
+				features = "src/test/resources/dsAlgo_FeatureFiles", 
+				glue= {"dsAlgo_StepDefinition", "dsAlgo_hooks" }) 
 		
 public class TestRunner extends AbstractTestNGCucumberTests{
-			/*@Override
-			@DataProvider(parallel = true)
-			public Object[][] scenarios()
-			{
-				return super.scenarios();
-			}	*/	
+	
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+				
+		return super.scenarios();
+    }
+				
 }
