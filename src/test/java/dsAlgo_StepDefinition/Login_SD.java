903ba54569df43c61e71cc811eb6652fcbc51673
Pushpa Banisetti <saisrinivasu6@gmail.com>
2025-03-18 20:26:32 -0400

    Updated Login per Review comments

--- src/test/java/dsAlgo_StepDefinition/Login_SD.java
+++ src/test/java/dsAlgo_StepDefinition/Login_SD.java
@@ -22,19 +22,19 @@ public class Login_SD {
         login_PF = new Login();
     }
 	
- @Given("The Login user is on the DS Algo Home Page")
+ @Given("The user is on the DS Algo Home Page for Login")
  public void the_user_is_on_the_ds_algo_home_page() {
      // Write code here that turns the phrase above into concrete actions
 		login_PF.openHome();		     
  }
 
- @When("The Login user should click the Sign in link")
+ @When("The user should click the Sign in link for Login")
  public void the_user_should_click_the_sign_in_link() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.ClickSignIn();
  }
 
- @Then("The Login user should be redirected to Sign in page")
+ @Then("The user should be redirected to Sign in page for Login")
  public void the_user_should_be_redirected_to_sign_in_page() {
      // Write code here that turns the phrase above into concrete actions
 	 String loginPgTitle = login_PF.getTitle();
@@ -42,13 +42,13 @@ public class Login_SD {
       
  }
 
- @Given("The Login user is on the DS Algo Sign in Page")
+ @Given("The user is on the DS Algo Sign in Page for Login")
  public void the_user_is_on_the_ds_algo_sign_in_page() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.openLogin();
  }
 
- @When("The Login user clicks login button after leaving the {string} textbox and {string} textbox empty")
+ @When("The user clicks login button after leaving the {string} textbox and {string} textbox empty")
  public void the_user_clicks_login_button_after_leaving_the_textbox_and_textbox_empty(String string, String string2) {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.ClickBtnLogin();
@@ -61,7 +61,7 @@ public class Login_SD {
 		Assert.assertEquals(string, ValidationMsg);
  }
  
-@When("The Login user clicks login button after entering the {string} and leaves {string} textbox empty")
+@When("The user clicks login button after entering the {string} and leaves {string} textbox empty")
  public void the_user_clicks_login_button_after_entering_the_and_leaves_textbox_empty(String string, String string2) {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.SetUserName(string);
@@ -76,14 +76,14 @@ public class Login_SD {
 	 Assert.assertEquals(string, ValidationMsg); 
  }
  
- @When("The Login user clicks login button after entering the Password only")
+ @When("The user clicks login button after entering the Password only")
  public void the_user_clicks_login_button_after_entering_the_password_only() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.SetPassword("password");
 	 login_PF.ClickBtnLogin();      
  }
 
- @When("The Login user clicks login button after entering invalid username and valid password")
+ @When("The user clicks login button after entering invalid username and valid password")
  public void the_user_clicks_login_button_after_entering_invalid_username_and_valid_password() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.SetUserName("Username");
@@ -91,14 +91,14 @@ public class Login_SD {
 	 login_PF.ClickBtnLogin();	 
  }
  
- @Then("The Login user should able to see an error message {string}.")
+ @Then("The user should able to see an error message {string}.")
  public void the_user_should_able_to_see_an_error_message(String string) {
      // Write code here that turns the phrase above into concrete actions	 
 	 Assert.assertEquals(string, login_PF.getErrMsg());
 	
  }
  
- @When("The Login user clicks login button after entering Valid username and Incorrect password")
+ @When("The user clicks login button after entering Valid username and Incorrect password")
  public void the_user_clicks_login_button_after_entering_valid_username_and_incorrect_password() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.SetUserName("bugbusters");
@@ -106,7 +106,7 @@ public class Login_SD {
 	 login_PF.ClickBtnLogin();		 
  }
 
- @When("The Login user clicks login button after entering valid username and valid password")
+ @When("The user clicks login button after entering valid username and valid password for Login")
  public void the_user_clicks_login_button_after_entering_valid_username_and_valid_password() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.SetUserName("bugbusters");
@@ -114,13 +114,13 @@ public class Login_SD {
 	 login_PF.ClickBtnLogin();	 
  }
 
- @Then("The Login user should land in Data Structure Home Page with message {string}")
+ @Then("The user should land in Data Structure Home Page with message {string} for Login")
  public void the_user_should_land_in_data_structure_home_page_with_message(String string) {
      // Write code here that turns the phrase above into concrete actions	 
 	 Assert.assertEquals(string, login_PF.getErrMsg()); 
 }
 
- @Given("The Login user is in the Home page after Sign in")
+ @Given("The user is in the Home page after Sign in for Login")
  public void the_user_is_in_the_home_page_after_sign_in() {
      // Write code here that turns the phrase above into concrete actions
 	 login_PF.openLogin();	 
@@ -129,40 +129,37 @@ public class Login_SD {
 	 login_PF.ClickBtnLogin();	 
  }
 
- @When("The Login user clicks {string}")
+ @When("The user clicks {string}")
  public void the_user_clicks(String string) {
      // Write code here that turns the phrase above into concrete actions	
 	 login_PF.ClickSignOut();
  }
 
- @Then("The Login user should be redirected to home page with message {string}")
+ @Then("The user should be redirected to home page with message {string}")
  public void the_user_should_be_redirected_to_home_page_with_message(String string) {
      // Write code here that turns the phrase above into concrete actions	
 	 Assert.assertEquals(string, login_PF.getErrMsg());
  }
  
- @When("Login User logs in with data from Excel {string}")
- public void login_user_logs_in_with_data_from_excel_for_row(String SheetName) throws IOException {
+ @When("User logs in with data from Excel {string} and {int} for Login")
+ public void login_user_logs_in_with_data_from_excel_for_row(String SheetName, int RowNo) throws IOException {
      List<Object[]> loginData = ExcelReader.readExcelData(SheetName);
+     int index = 1;
      for (Object[] row : loginData) { 
-         username = (String) row[0];
-         password = (String) row[1];
-         expectedResult = (String) row[2];         
-         performLogin(username, password, expectedResult);
+    	 if (index == RowNo)
+    	 {
+	         username = (String) row[0];
+	         password = (String) row[1];
+	         expectedResult = (String) row[2];         
+	         performLogin(username, password, expectedResult);
+    	 }
+         index++;
      }
  }
 
- @Then("Login User should see the either Log in Success or Failure")
+ @Then("User should see the either Log in Success or Failure")
  public void login_user_should_see_the_either_log_in_sucess_or_failure() {
-     // Assert the expected result	 
- }
-
- private void performLogin(String username, String password, String expectedResult) {
-     //Implementation of the Login functionality.
-	 login_PF.SetUserName(username);
-	 login_PF.SetPassword(password);
-	 login_PF.ClickBtnLogin();		
-	 
+     // Assert the expected result
 	 if (expectedResult.equals("Please fill out this field."))
 	 {
 		 if (username.equals(""))
@@ -178,10 +175,13 @@ public class Login_SD {
 	 {
 		 Assert.assertEquals(expectedResult, login_PF.getErrMsg());
 	 }
-	 
-	 login_PF.openLogin();
-	 login_PF = new Login();
  }
- 
+
+ private void performLogin(String username, String password, String expectedResult) {
+     //Implementation of the Login functionality.
+	 login_PF.SetUserName(username);
+	 login_PF.SetPassword(password);
+	 login_PF.ClickBtnLogin();
+ } 
 
 }
